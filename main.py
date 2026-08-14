eeab3cb3b4dcb3aa10fbfed685214f15f19894bc
jhederith <jhederith@gmail.com>
2026-08-13 21:30:31 -0500

    remove no needed files, add comment description, ready to test by Oscar and Esteban

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+"""
+Trabajo 1: Lectura de datos y análisis descriptivo
+Aprendeizaje Automático y Minería de Datos
+Dataset: Bike Sharing (UC Irvine Machine Learning Repository)
+Varaible respuesta: cnt (total de alquileres de bicicletas)
+Autores:
+   - Esteban Quitian @quitian07
+   - Oscar Beltran @oscarbeltran12
+   - Jhederith Quitian @jhederith
+"""
+
 import sys
 import os
 from tabulate import tabulate
--- src/stats.py
+++ src/stats.py
@@ -1,4 +1,4 @@
-mport matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import os
 
 def analizar_distribuciones(df):
