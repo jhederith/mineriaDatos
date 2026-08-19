7cbeee9a95dfda5f3e92599338dac5e600de0880
jhederith <jhederith@gmail.com>
2026-08-19 08:36:16 -0500

    Update changes

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ def main():
     # 3. Análisis de Correlaciones (Calcula coeficientes y genera los 2 gráficos bivariados)
     analizar_correlaciones(df)
 
-    #4. Demanda por hora
+    #4. Demanda por hora, se encuentra en src/correlations
     demanda_por_hora(df)
 
     print("\n" + "=" * 70)
