0d74a5354d3e6a5cbcf66f6e0b3485d119a06638
jhederith <jhederith@gmail.com>
2026-08-19 20:04:12 -0500

    last tested working file

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'src')))
 
 from loader import cargar_y_preparar_datos
 from stats import analizar_distribuciones
-from correlations import analizar_correlaciones, demanda_por_hora
+from correlations import analizar_correlaciones, demanda_por_hora, analizar_matriz_correlacion
 
 def main():
     print("=" * 70)
@@ -41,6 +41,9 @@ def main():
 
     #4. Demanda por hora, se encuentra en src/correlations
     demanda_por_hora(df)
+
+    #5. Matríz de correlación
+    analizar_matriz_correlacion(df)
 
     print("\n" + "=" * 70)
     print(" Proceso completado con éxito.")
